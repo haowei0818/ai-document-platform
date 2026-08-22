d50c1d72c5cd0a88750117436ab1b87a957338e9
haowei0818 <lovegjo3au@gmail.com>
2026-08-22 16:29:31 +0800

    新增 documents App 與 Document model

---
+++ documents/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Document(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='documents/')
+    title = models.CharField(max_length=255)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    
