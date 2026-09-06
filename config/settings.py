c5c82e251746c56e3db627d9ac787f8f8274ad8a
haowei0818 <lovegjo3au@gmail.com>
2026-09-06 16:46:10 +0800

    新增 Documents App、Document model 並註冊到後台，設定獨立 media 資料夾存放上傳檔案

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,5 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- documents/models.py
+++ documents/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Document(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='documents/')
+    file = models.FileField(upload_to='uploads/')
     title = models.CharField(max_length=255)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
